bc1c574f2120006e9cbadd1ef48bcc7c70986d9b
solvery@163.com <solvery@163.com>
2016-07-12 10:10:51 +0800

    matlab

--- file_read_1.m
+++ file_read_1.m
@@ -1,4 +1,10 @@
 
 [d0, d1, d2, d3] = textread('file_read_1_data.txt', '%s %s %s %s', 'headerlines', 1);
 
+fd0 = typecast(uint32(hex2dec(d0)),'single');
+fd1 = typecast(uint32(hex2dec(d1)),'single');
+fd2 = typecast(uint32(hex2dec(d2)),'single');
+fd3 = typecast(uint32(hex2dec(d3)),'single');
 [d0, d1, d2, d3] 
+[df0, df1, df2, df3] 
+
