a169dbc777f9b387d808349ab767f76ee160bd2d
solvery@163.com <solvery@163.com>
2016-07-13 15:24:36 +0800

    matlab

--- matrix_1.m
+++ matrix_1.m
@@ -19,3 +19,12 @@ rep=a*ones(1,n)
 a=[1 4 8];
 rep=a(ones(1,n),:)
 rep=ones(n,1)*a
+
+A=[1,2,3;4,5,6];
+m=size(A,1)
+n=size(A,2)
+M=3; N=2;
+mIndex=(1:m)'*ones(1,M);
+nIndex=(1:n)'*ones(1,N);
+B=A(mIndex,nIndex)
+
