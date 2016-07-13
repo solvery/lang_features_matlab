1da4f960b5699187823e56729ad418c962f16605
solvery@163.com <solvery@163.com>
2016-07-13 15:19:30 +0800

    matlab

--- time_2.m
+++ time_2.m
@@ -1,10 +1,11 @@
 
 
-profile on -detail mmex% 啟動計時器（只對M/MEX 檔案進行計時）
+profile on -detail builtin 
+%profile on -detail mmex
 for i= 1:1000
-	a = inv(rand(100));% 計算100x100 亂數矩陣的反矩陣
-	b = mean(rand(100));% 計算100x100 亂數矩陣的每一直行平均值
+	a = inv(rand(100));
+	b = mean(rand(100));
 end
-profile off% 停止計時器
-profile report% 呈現計時結果
+profile off
+profile report
 
