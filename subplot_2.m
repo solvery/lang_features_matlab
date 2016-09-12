0fb0e6ca31fbe6f07157662349ef6e484c15b094
solvery@163.com <solvery@163.com>
2016-09-12 14:28:18 +0800

    lang_features_matlab

---
+++ subplot_2.m
@@ -0,0 +1,14 @@
+clc;
+clear;
+
+figure('Name','默认');
+subplot(2,2,1);
+subplot(2,2,2);
+subplot(2,2,3);
+subplot(2,2,4);
+
+figure('Name','紧凑');
+subplot('Position',[0.02 0.65 0.3 0.3]);
+subplot('Position',[0.35 0.65 0.3 0.3]);
+subplot('Position',[0.02 0.3 0.3 0.3]);
+subplot('Position',[0.35 0.3 0.3 0.3]);
---
+++ subplot_3.m
@@ -0,0 +1,12 @@
+clc;
+clear;
+close;
+
+figure;
+ha = tight_subplot(3,2,[.01 .03],[.1 .01],[.01 .01])
+for ii = 1:6; 
+    axes(ha(ii)); 
+    plot(randn(10,ii)); 
+end
+set(ha(1:4),'XTickLabel',''); 
+set(ha,'YTickLabel',''v
