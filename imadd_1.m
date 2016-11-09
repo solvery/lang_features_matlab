d0efd9b5b8dbfe706bfaa965e8d448491f57d9d1
solvery@163.com <solvery@163.com>
2016-11-09 20:28:32 +0800

    /home/yang/workspace/verdiLog/lang_features_matlab at lang_features_matlab

--- imadd_1.m
+++
@@ -1,9 +0,0 @@
-
-% Add two images together and specify an output class.
-I = imread('rice.png');
-J = imread('cameraman.tif');
-K = imadd(I,J,'uint16');
-imshow(K,[])
-
-img_out = I*0.5 + J*0.5;
-imshow(img_out)
