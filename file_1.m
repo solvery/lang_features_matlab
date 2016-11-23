81141f3091665a2bef45e01a42f3ad9050476447
solvery@163.com <solvery@163.com>
2016-11-23 16:40:52 +0800

    /home/yang/workspace/verdiLog/lang_features_matlab at lang_features_matlab

--- file_1.m
+++ file_1.m
@@ -3,3 +3,4 @@ A=[1,2,3];
 fd_out = fopen('/tmp/fn_out', 'wt');
 fwrite(fd_out, A, 'uint8');
 fclose(fd_out);
+
---
+++ file_2.m
@@ -0,0 +1,8 @@
+
+for i = 1:3
+    disp(i);
+    A=[1,2,3];
+    filename = ['/tmp/fn_out_'  num2str(i,'%02d')];
+    disp(filename);
+end
+
